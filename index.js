e1d709f4f0d215fc8ece77621346b3f63c9c9253
lehao98 <75057869+lehao98@users.noreply.github.com>
2021-03-07 00:41:09 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function j(){
+var name=prompt("Hi, Welcome to Programming class! What is your name?");
+document.getElementById("po").innerHTML="Hello! " + name + " ! My name is Lehao Ruan.";
+
+}
+function study(){
+   alert("OKAY, YOU FAIL!");
+ }
+j();
+
+ function futr() {
+ var in1=document.futureInput.in1.value;
+ var in2=document.futureInput.in2.value;
+alert(in1 + " " + in2 + " " );
+console.log(in1,in2);
+}
